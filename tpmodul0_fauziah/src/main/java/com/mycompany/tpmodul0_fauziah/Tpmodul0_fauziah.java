b857c52618810b163d89708c616518d15e68e671
pauciaa <fauziahadinda11@gmail.com>
2024-10-13 20:44:43 +0700

    initial commit

---
+++ tpmodul0_fauziah/src/main/java/com/mycompany/tpmodul0_fauziah/Tpmodul0_fauziah.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tpmodul0_fauziah;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Tpmodul0_fauziah {
+
+    public static void main(String[] args) {
+        System.out.println("Goodby, World");
+    }
+}
